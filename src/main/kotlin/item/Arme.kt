5e7c106db4f1ec61b9109571e329a9cc5054f9f8
Moulin Timothée <timomoulin@msn.com>
2023-09-23 10:35:22 +0200

    TirageDes et commentaire sur le calcul des dégat de l'arme

--- src/main/kotlin/item/Arme.kt
+++ src/main/kotlin/item/Arme.kt
@@ -1,5 +1,6 @@
 package item
 
+import jeu.TirageDes
 import personnage.Personnage
 
 class Arme(
@@ -11,14 +12,22 @@ class Arme(
     // Méthode pour calculer les dégâts de l'arme
     fun calculerDegats(): Int {
         // Exemple : 1d6 +2 ( cad un nombre entre 1 et 6 plus le modificateur 2)
-        val resultatLancer = (type.degatMin..type.degatMax).random() + qualite.bonusRarete
-        if (resultatLancer == type.degatMax) {
+        //      On tire 1 dè de 6 (min: 1 max:6 )
+        //      Si on tire 6 alors on multiplie par le multiplicateur de critique du type de l'arme ( par exemple 3)
+        //      Sinon on garde le nombre du tirage tel quelle
+        //      On ajoute le bonus de qualite au dégat ici 2
+
+        // Instanciation d'un tirage de dés
+        val deDegat= TirageDes(type.nombreDes,type.valeurDeMax)
+        // on lance les dés
+        var resultatLancer = deDegat.lance()
+        if (resultatLancer == type.valeurDeMax) {
             // Coup critique (si le nombre tiré correspond au maximum)
             println("Coup critique !")
-            return resultatLancer * type.multiplicateurCritique
-        } else {
-            return resultatLancer
+           resultatLancer= resultatLancer * type.multiplicateurCritique
         }
+        resultatLancer += this.qualite.bonusRarete
+        return resultatLancer
     }
 
     override fun utiliser(cible: Personnage) {
@@ -26,7 +35,7 @@ class Arme(
     }
 
     override fun toString(): String {
-        return "${qualite.couleur} ${type.nom} ${qualite.nom}  Dégâts :${type.degatMin}d${type.degatMax} +${qualite.bonusRarete}  \u001B[0m"
+        return "${qualite.couleur} ${type.nom} ${qualite.nom}  Dégâts :${type.nombreDes}d${type.valeurDeMax} +${qualite.bonusRarete}  \u001B[0m"
 
     }
 }
--- src/main/kotlin/item/Bombe.kt
+++ src/main/kotlin/item/Bombe.kt
@@ -1,14 +1,14 @@
 package item
 
-import jeu.De
+import jeu.TirageDes
 import personnage.Personnage
 
 class Bombe (nom:String, description:String ,val nombreDeDes:Int, val maxDe: Int):Item(nom,description) {
     override fun utiliser(cible: Personnage) {
-        val deDegat= De(nombreDeDes,maxDe)
+        val deDegat= TirageDes(nombreDeDes,maxDe)
         var degat=deDegat.lance()
         degat = maxOf(1,degat-cible.calculeDefense())
-        cible.pointDeVie -= degat;
+        cible.pointDeVie -= degat
         println("La/Le ${this.nom} inflige $degat points de dégâts")
     }
 }
